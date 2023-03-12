0c2754f9c1a89dbf1c17c10212695a8cb648e9df
Vince <vincivenv@gmail.com>
2023-03-12 22:49:30 +0100

    checkpoint

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::{io, sync::Arc};
+
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_lab::extract::Path;
+
+use std::time::Duration;
+
+use actix_web::rt::time::interval;
+use actix_web_lab::sse::{self, ChannelStream, Sse};
+use futures_util::future;
+use parking_lot::Mutex;
+
+struct Broadcaster {
+    inner: Mutex<BroadcasterInner>,
+}
+
+#[derive(Debug, Clone, Default)]
+struct BroadcasterInner {
+    clients: Vec<sse::Sender>,
+}
+
+impl Broadcaster {
+    /// Constructs new broadcaster and spawns ping loop.
+    fn create() -> Arc<Self> {
+        let this = Arc::new(Broadcaster {
+            inner: Mutex::new(BroadcasterInner::default()),
+        });
+
+        Broadcaster::spawn_ping(Arc::clone(&this));
+
+        this
+    }
+
+    /// Pings clients every 10 seconds to see if they are alive and remove them from the broadcast
+    /// list if not.
+    fn spawn_ping(this: Arc<Self>) {
+        actix_web::rt::spawn(async move {
+            let mut interval = interval(Duration::from_secs(10));
+
+            loop {
+                interval.tick().await;
+                this.remove_stale_clients().await;
+            }
+        });
+    }
+
+    /// Removes all non-responsive clients from broadcast list.
+    async fn remove_stale_clients(&self) {
+        let clients = self.inner.lock().clients.clone();
+
+        let mut ok_clients = Vec::new();
+
+        for client in clients {
+            if client
+                .send(sse::Event::Comment("ping".into()))
+                .await
+                .is_ok()
+            {
+                ok_clients.push(client.clone());
+            }
+        }
+
+        self.inner.lock().clients = ok_clients;
+    }
+
+    /// Registers client with broadcaster, returning an SSE response body.
+    async fn new_client(&self) -> Sse<ChannelStream> {
+        let (tx, rx) = sse::channel(10);
+
+        tx.send(sse::Data::new("connected")).await.unwrap();
+
+        self.inner.lock().clients.push(tx);
+
+        rx
+    }
+
+    /// Broadcasts `msg` to all clients.
+    async fn broadcast(&self, msg: &str) {
+        let clients = self.inner.lock().clients.clone();
+
+        let send_futures = clients
+            .iter()
+            .map(|client| client.send(sse::Data::new(msg)));
+
+        // try to send to all clients, ignoring failures
+        // disconnected clients will get swept up by `remove_stale_clients`
+        let _ = future::join_all(send_futures).await;
+    }
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let data = Broadcaster::create();
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(Arc::clone(&data)))
+            .service(event_stream)
+            .service(broadcast_msg)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .workers(2)
+    .run()
+    .await
+}
+
+#[get("/events")]
+async fn event_stream(broadcaster: web::Data<Broadcaster>) -> impl Responder {
+    broadcaster.new_client().await
+}
+
+#[post("/broadcast/{msg}")]
+async fn broadcast_msg(
+    broadcaster: web::Data<Broadcaster>,
+    Path((msg,)): Path<(String,)>,
+) -> impl Responder {
+    broadcaster.broadcast(&msg).await;
+    HttpResponse::Ok().body("msg sent")
+}
